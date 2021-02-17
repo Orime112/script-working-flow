000b13b4bf2e58614c6125abed3f14ed8e5567db
Orime小猪 <1126439586@qq.com>
2021-02-17 17:11:58 +0800

    文档(完善文档): 完善文档

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-
 let a = 12
 
 console.log(a)
+
+// function _x() {
+// 	let a = 23
+// 	const b = 34
+// 	return b + a
+// }
+
+// function _d() {
+// 	return _d + 0
+// }
+
+// let p = [1, 3, 534, 7654, 234, 234]
